e04d624d935c5a052e45735dd15cf5c0a4cc2436
skniro <46392259+skniro@users.noreply.github.com>
2024-12-08 16:36:56 +0800

    Set another side block hopper set and lock output slot.

--- Fabric/src/main/java/com/skniro/growable_ores_extension/datagen/GrowableModelProvider.java
+++ Fabric/src/main/java/com/skniro/growable_ores_extension/datagen/GrowableModelProvider.java
@@ -1,10 +1,9 @@
 package com.skniro.growable_ores_extension.datagen;
 
-import com.skniro.growable_ores_extension.block.GrowableOresBlocks;
+import net.fabricmc.fabric.api.client.datagen.v1.provider.FabricModelProvider;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
-import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
-import net.minecraft.data.client.BlockStateModelGenerator;
-import net.minecraft.data.client.ItemModelGenerator;
+import net.minecraft.client.data.BlockStateModelGenerator;
+import net.minecraft.client.data.ItemModelGenerator;
 
 public class GrowableModelProvider extends FabricModelProvider {
     public GrowableModelProvider(FabricDataOutput dataGenerator){
