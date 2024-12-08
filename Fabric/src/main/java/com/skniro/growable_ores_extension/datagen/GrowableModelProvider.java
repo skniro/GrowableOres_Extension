c3c14ed05de85246dc856fa86d4ae1e55a629fbb
skniro <46392259+skniro@users.noreply.github.com>
2024-12-08 16:50:01 +0800

    Revert "Set another side block hopper set and lock output slot."
    This reverts commit e04d624d935c5a052e45735dd15cf5c0a4cc2436.

--- Fabric/src/main/java/com/skniro/growable_ores_extension/datagen/GrowableModelProvider.java
+++ Fabric/src/main/java/com/skniro/growable_ores_extension/datagen/GrowableModelProvider.java
@@ -1,9 +1,10 @@
 package com.skniro.growable_ores_extension.datagen;
 
-import net.fabricmc.fabric.api.client.datagen.v1.provider.FabricModelProvider;
+import com.skniro.growable_ores_extension.block.GrowableOresBlocks;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
-import net.minecraft.client.data.BlockStateModelGenerator;
-import net.minecraft.client.data.ItemModelGenerator;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
+import net.minecraft.data.client.BlockStateModelGenerator;
+import net.minecraft.data.client.ItemModelGenerator;
 
 public class GrowableModelProvider extends FabricModelProvider {
     public GrowableModelProvider(FabricDataOutput dataGenerator){
